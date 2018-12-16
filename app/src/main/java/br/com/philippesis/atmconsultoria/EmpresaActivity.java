17a9e65ff8aea9f5a89c59de5a02662810e0180a
rogerphilippe@outlook.com <Um9nZXI>
2018-12-16 19:12:04 -0200

    Tela A Empresa.

---
+++ app/src/main/java/br/com/philippesis/atmconsultoria/EmpresaActivity.java
@@ -0,0 +1,43 @@
+package br.com.philippesis.atmconsultoria;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class EmpresaActivity extends AppCompatActivity {
+
+    private TextView tvAEmpresa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empresa);
+
+        tvAEmpresa = (TextView) findViewById(R.id.idtvAEmpresa);
+
+        tvAEmpresa.setText("Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - " +
+                "Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD,Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD,Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD,Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves -" +
+                " A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves -" +
+                " A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD,Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves -" +
+                " A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD, Chaves - " +
+                "A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD,Chaves - A Venda da Vila, Parte 1 (1976) - Alta Qualidade SBT HD.");
+
+    }
+}
--- app/src/main/java/br/com/philippesis/atmconsultoria/MainActivity.java
+++ app/src/main/java/br/com/philippesis/atmconsultoria/MainActivity.java
@@ -1,13 +1,30 @@
 package br.com.philippesis.atmconsultoria;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ivEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivEmpresa = (ImageView) findViewById(R.id.idivEmpresa);
+
+        ivEmpresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, EmpresaActivity.class));
+
+            }
+        });
+
     }
 }
